f80c96b3ccfad2fe6604ca1fcb940e16385cd26d
adhambadawi <adhambadawi857@gmail.com>
2024-02-03 15:36:53 -0500

    ElevatorSubsystem initial code

--- src/ElevatorSubsystem.java
+++ src/ElevatorSubsystem.java
@@ -4,4 +4,31 @@
 
 public class ElevatorSubsystem {
 
+    private Scheduler scheduler;
+    private int elevatorId;
+    private int currentFloor;
+    
+    
+    
+    
+    /**
+     * Constructor for ElevatorSubsystem thread
+     * @param scheduler The elevator scheduler
+     */
+    public ElevatorSubsystemystem(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+    
+    public int getElevatorId() {
+        return elevatorId;
+    }
+    
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+    
+    
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
 }
