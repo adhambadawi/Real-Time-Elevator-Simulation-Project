fbb0c394a5d3d7cac00e731048223eb9c04f5cd5
Adham Badawi <adhambadawi@cmail.carleton.ca>
2024-04-10 14:31:56 -0400

    final iteration adjustments, GUI and input file updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     private static final int NUM_ELEVATOR_CARS = 4;
-    private static final int NUM_FLOOR_ARRIVAL_FAULT_CARS = 0;
-    private static final int NUM_DOOR_FAULT_CARS = 0;
+    private static final int NUM_FLOOR_ARRIVAL_FAULT_CARS = 1;
+    private static final int NUM_DOOR_FAULT_CARS = 1;
     private static final int TOTAL_ELEVATOR_CARS = NUM_ELEVATOR_CARS + NUM_FLOOR_ARRIVAL_FAULT_CARS + NUM_DOOR_FAULT_CARS;
     public static void main(String[] args) {
         Scheduler scheduler = Scheduler.getScheduler();
         //Elevator and floor threads
         Thread floorThread;
         ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
-        FloorSubsystem floorSubSystem = new FloorSubsystem("../ElevatorCalls");
+        FloorSubsystem floorSubSystem = new FloorSubsystem("ElevatorCalls");
         floorThread = new Thread(floorSubSystem);
 
         ElevatorSubsystemGui gui = new ElevatorSubsystemGui(TOTAL_ELEVATOR_CARS, 22);
